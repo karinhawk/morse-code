ddb32f6392b6b7b88ad2075868288fb9e1793930
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-09-02 10:04:57 +0100

    finished tests

---
+++ morse.js
@@ -0,0 +1,3 @@
+export default morseArray;
+
+//have array of morse letters and english counterpart
--- translator.spec.js
+++ translator.spec.js
@@ -11,6 +11,16 @@ describe('Testing translateToMorse', () => {
         expect(result).toBe(".-")
     })
 
+    it("A should translate to be .-", () => {
+        //arrange
+        let string;
+        //act
+        string = "A";
+        let result = translateToMorse(string)
+        //assert
+        expect(result).toBe(".-")
+    })
+
     it("should translate Rob to be .-. --- -...", () => {
         //arrange
         let string;
@@ -39,6 +49,42 @@ describe('Testing translateToMorse', () => {
         //assert
         expect(result).toBe(".... . .-.. .-.. --- / - .... . .-. .")
     })
+    it("should translate 'hello there ' to be .... . .-.. .-.. --- / - .... . .-. .", () => {
+        //arrange
+        let string;
+        //act
+        string = "hello there ";
+        let result = translateToMorse(string)
+        //assert
+        expect(result).toBe(".... . .-.. .-.. --- / - .... . .-. .")
+    })
+    it("should translate '!' to be", () => {
+        //arrange
+        let string;
+        //act
+        string = "!";
+        let result = translateToMorse(string)
+        //assert
+        expect(result).toBe("-.-.--")
+    })
+    it("should translate '1' to be .----", () => {
+        //arrange
+        let string;
+        //act
+        string = "1";
+        let result = translateToMorse(string)
+        //assert
+        expect(result).toBe(".----")
+    })
+    it("should translate '123' to be .---- ..--- ...--", () => {
+        //arrange
+        let string;
+        //act
+        string = "123";
+        let result = translateToMorse(string)
+        //assert
+        expect(result).toBe(".---- ..--- ...--")
+    })
 })
 
 //with dom EXTRACT EVERYTHING from javascript, set as data type please :)
@@ -46,10 +92,9 @@ describe('Testing translateToMorse', () => {
 //what should translateToMorse actually do
 // - receive letters and translate those letters into morse code - DONE
 // - take into consideration the capital letters- turn all inputs into uppercase :) there is no capital or lower in morse - DONE
-// - take into consideration spaces in words and translate accordingly
-// - numbers
-// - symbols
-// - language?
+// - take into consideration spaces in words and translate accordingly DONE
+// - numbers DONE
+// - symbols DONE
 
 //manipulateDOM
 // - output translation into display 
